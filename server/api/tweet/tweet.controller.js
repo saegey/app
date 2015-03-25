205b42ecc368eebab0df37ba68159ccec25e3e5b
Adam Saegebarth <adams@hasoffers.com>
2015-03-24 22:02:28 -0700

    clean up tweet controller

--- server/api/tweet/tweet.controller.js
+++ server/api/tweet/tweet.controller.js
@@ -12,7 +12,7 @@ exports.index = function (req, res) {
   res.send("awesome");
 };
 
-exports.hashtags = function (req, res) {
+exports.hashtags = function(req, res) {
   var sheetService = new GoogleSheetService('https://docs.google.com/spreadsheets/d/1azduyest2um3zrUJFvS5upGa2LO7cReg0hob6VtGCas/export?gid=0&format=csv');
   sheetService.getData(function (result) {
     res.send(result);
